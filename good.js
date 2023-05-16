db718da03164dead6a82ed5a1969d8695975ae8c
edxghbj <kayodeolasunkanmi75@gmail.com>
2023-05-16 06:58:44 -0700

    james

---
+++ good.js
@@ -0,0 +1,10 @@
+let a= new Date(). getHours();
+if(a>00 && a<11){
+  alert('Good Morning')
+ }
+  else if(a>11 && a<16){
+     alert('Good afternoon')
+ }
+ else{
+     alert('Good Evening')
+ }
---
+++ ss.js
@@ -0,0 +1,10 @@
+let resturant={
+    name: 'chicken Republic',
+    categories: ['aficandish','chinesedish','calarbadish','indiadish','drink','soup','meat',],
+    drink: ['chivita','chiexotic','cocoa','fanta','7up','evabottlewater',],
+    soup:['egusi','ewedu','stew','ogbono','okoro',],
+    swallow:['amala','semovita','akpu','fufu','eba',],
+    owner: 'mr okoro',
+    location: 'bodija',
+};
+alert(`i want ${resturant.categories[5]} ${resturant.soup[0]} and ${resturant.swallow[4]} i also want ${resturant.drink[3]}, thanks`);
